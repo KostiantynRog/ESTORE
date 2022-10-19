d968b9d0329320604ecd4f839cf3d2935558e765
Kostiantyn Rozhkov <krozhkov09@gmail.com>
2022-10-19 21:02:07 +0200

    ESTORE-17

--- src/main/java/com/rog/EShop/controllers/StatsController.java
+++ src/main/java/com/rog/EShop/controllers/StatsController.java
@@ -2,6 +2,7 @@ package com.rog.EShop.controllers;
 
 import com.rog.EShop.dto.StatsDto;
 import com.rog.EShop.services.StatsService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class StatsController {
     public StatsController(StatsService statsService) {
         this.statsService = statsService;
     }
-
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping(path = "/stats")
     public List<StatsDto> statsDtoList() {
         return statsService.getStats();
--- src/main/java/com/rog/EShop/security/WebSecurityConfig.java
+++ src/main/java/com/rog/EShop/security/WebSecurityConfig.java
@@ -1,12 +1,23 @@
 package com.rog.EShop.security;
 
+import com.nimbusds.jose.shaded.json.JSONArray;
+import com.nimbusds.jose.shaded.json.JSONObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -14,13 +25,51 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
                 .authorizeHttpRequests((requests) -> requests
-                        .antMatchers("/**").permitAll()
-                        .anyRequest().authenticated()
+                        .antMatchers("/api/users/register").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(resourceServerConfigurer -> resourceServerConfigurer
+                        .jwt(jwtConfigurer -> jwtConfigurer
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter()))
                 );
+
 
         return http.build();
     }
+    @Bean
+    public Converter<Jwt, AbstractAuthenticationToken> jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter());
+        return jwtAuthenticationConverter;
+    }
 
+    @Bean
+    public Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter() {
+        JwtGrantedAuthoritiesConverter delegate = new JwtGrantedAuthoritiesConverter();
+
+        return new Converter<>() {
+            @Override
+            public Collection<GrantedAuthority> convert(Jwt jwt) {
+                Collection<GrantedAuthority> grantedAuthorities = delegate.convert(jwt);
+
+                if (jwt.getClaim("realm_access") == null) {
+                    return grantedAuthorities;
+                }
+                JSONObject realmAccess = jwt.getClaim("realm_access");
+                if (realmAccess.get("roles") == null) {
+                    return grantedAuthorities;
+                }
+                JSONArray roles = (JSONArray) realmAccess.get("roles");
+
+                final List<SimpleGrantedAuthority> keycloakAuthorities = roles
+                        .stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                        .collect(Collectors.toList());
+                grantedAuthorities.addAll(keycloakAuthorities);
+
+                return grantedAuthorities;
+            }
+        };
+    }
 }
