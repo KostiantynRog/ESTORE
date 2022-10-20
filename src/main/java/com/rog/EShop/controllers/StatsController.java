78be7898d3e1793f74e82e73140765dd1b3cb98d
Kostiantyn Rozhkov <krozhkov09@gmail.com>
2022-10-20 17:23:16 +0200

    ESTORE-17 (without bug in roles)

--- src/main/java/com/rog/EShop/controllers/StatsController.java
+++ src/main/java/com/rog/EShop/controllers/StatsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -17,6 +18,7 @@ public class StatsController {
     public StatsController(StatsService statsService) {
         this.statsService = statsService;
     }
+
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping(path = "/stats")
     public List<StatsDto> statsDtoList() {
--- src/main/java/com/rog/EShop/security/WebSecurityConfig.java
+++ src/main/java/com/rog/EShop/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.GrantedAuthority;
@@ -21,6 +22,7 @@ import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
 
     @Bean
@@ -64,7 +66,7 @@ public class WebSecurityConfig {
 
                 final List<SimpleGrantedAuthority> keycloakAuthorities = roles
                         .stream()
-                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                        .map(role -> new SimpleGrantedAuthority("" + role))
                         .collect(Collectors.toList());
                 grantedAuthorities.addAll(keycloakAuthorities);
 
