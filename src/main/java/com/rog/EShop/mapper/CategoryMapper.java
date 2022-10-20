a4c7ac3921a7995ca2bcefc4ace9ab5956f43d25
Kostiantyn Rozhkov <42935404+KostiantynRog@users.noreply.github.com>
2022-10-20 17:48:49 +0200

    ESTORE-17 (#12)
    * ESTORE-17
    
    * ESTORE-17 (without bug in roles)
    
    * ESTORE-17 (without bug in roles)
    
    * ESTORE-17 (without bug in roles)

--- src/main/java/com/rog/EShop/mapper/CategoryMapper.java
+++ src/main/java/com/rog/EShop/mapper/CategoryMapper.java
@@ -3,7 +3,6 @@ package com.rog.EShop.mapper;
 import com.rog.EShop.dto.CategoryDto;
 import com.rog.EShop.entity.Category;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
